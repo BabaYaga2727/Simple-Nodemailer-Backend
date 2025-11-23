2a6a6117bb0f843e744b6862429407b7b21fa1b7
Surafel Tezera <surafel.tezera@aastustudent.edu.et>
2025-11-23 10:50:43 +0300

    Update

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://simple-nodemailer-frontend-f9xv.vercel.app/"
+    origin: "https://simple-nodemailer-frontend-f9xv.vercel.app/",
+    methods: ["GET", "POST"]
 }))
 
 
